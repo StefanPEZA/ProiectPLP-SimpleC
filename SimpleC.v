cbd14df23cfe6e0fb4fd26d42bd7a62e3417c86a
Stefan Pezamosca <stefan2014pe@gmail.com>
2020-12-12 14:43:22 +0200

    Adaugat SimpleC.v + tipuri si expresii algerbrice/booleene

---
+++ SimpleC.v
@@ -0,0 +1,76 @@
+Require Import Strings.String.
+Require Import Coq.ZArith.BinInt.
+Local Open Scope string_scope. 
+Local Open Scope list_scope.
+Local Open Scope Z_scope.
+Scheme Equality for string.
+
+Inductive newString : Type :=
+| errString : newString
+| stringVal : string -> newString.
+
+Inductive newNat : Type :=
+| errNat : newNat
+| natVal : nat -> newNat.
+
+Inductive newInt : Type :=
+| errInt : newInt
+| intVal : Z -> newInt.
+
+Inductive newBool : Type :=
+| errBool : newBool
+| boolVal : bool -> newBool.
+
+Inductive newPointer : Type :=
+| NULL : newPointer
+| pointerVal : nat -> newPointer.
+
+Notation "'str(' S ')'" := (stringVal S) (at level 0).
+Coercion intVal : Z >-> newInt.
+Coercion natVal : nat >-> newNat.
+Coercion boolVal : bool >-> newBool.
+Coercion pointerVal : nat >-> newPointer.
+
+Inductive newType : Type :=
+| err_undeclared : newType
+| err_assignment : newType
+| default : newType
+| natType : newNat -> newType
+| intType : newInt -> newType
+| boolType : newBool -> newType
+| stringType : newString -> newType
+| pointType : newPointer -> newType.
+
+Inductive Exp:=
+| n_const : newNat -> Exp
+| i_const : newInt -> Exp
+| b_const : newBool -> Exp
+| avar : string -> Exp
+| adunare : Exp -> Exp -> Exp
+| scadere : Exp -> Exp -> Exp
+| inmultire : Exp -> Exp -> Exp
+| impartire : Exp -> Exp -> Exp
+| modulo : Exp -> Exp -> Exp
+| putere : Exp -> Exp -> Exp
+| negatie : Exp -> Exp
+| si_logic : Exp -> Exp -> Exp
+| sau_logic : Exp -> Exp -> Exp
+| mai_mic : Exp -> Exp -> Exp
+| mai_micEq : Exp -> Exp -> Exp
+| mai_mare : Exp-> Exp -> Exp
+| mai_mareEq : Exp -> Exp -> Exp
+| egalitate : Exp -> Exp -> Exp
+| inegalitate : Exp -> Exp -> Exp
+| sau_exclusiv : Exp -> Exp -> Exp
+| si_exclusiv: Exp -> Exp -> Exp.
+
+Coercion n_const : newNat >-> Exp.
+Coercion i_const : newInt >-> Exp.
+Coercion b_const : newBool >-> Exp.
+Coercion avar : string >-> Exp.
+
+
+
+
+
+
