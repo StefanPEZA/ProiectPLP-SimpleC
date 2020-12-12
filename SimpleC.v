a1a5821ee374bf14e4ac8e190c177ec4f5e44ec5
Stefan Pezamosca <stefan2014pe@gmail.com>
2020-12-12 15:13:40 +0200

    Am adaugat notatii pentru expresii algebrice/booleene

--- SimpleC.v
+++ SimpleC.v
@@ -69,6 +69,27 @@ Coercion i_const : newInt >-> Exp.
 Coercion b_const : newBool >-> Exp.
 Coercion avar : string >-> Exp.
 
+(*Notatii expresii algebrice*)
+Notation "A +' B" := (adunare A B)(at level 50, left associativity).
+Notation "A -' B" := (scadere A B)(at level 50, left associativity).
+Notation "A *' B" := (inmultire A B)(at level 48, left associativity).
+Notation "A /' B" := (impartire A B)(at level 48, left associativity).
+Notation "A %' B" := (modulo A B)(at level 48, left associativity).
+Notation "A ^' B" := (putere A B) (at level 48, left associativity).
+
+(*Notatii expresii booleene*)
+Notation "!' A" := (negatie A) (at level 45, right associativity).
+Notation "A &&' B" := (si_logic A B) (at level 55, left associativity).
+Notation "A 'xand' B" := (si_exclusiv A B) (at level 55, left associativity).
+Notation "A ||' B" := (si_logic A B) (at level 56, left associativity).
+Notation "A 'xor' B" := (sau_exclusiv A B) (at level 56, left associativity).
+Notation "A <=' B" := (mai_micEq A B) (at level 52, left associativity).
+Notation "A <' B" := (mai_mic A B) (at level 52, left associativity).
+Notation "A >=' B" := (mai_micEq A B) (at level 52, left associativity).
+Notation "A >' B" := (mai_mic A B) (at level 52, left associativity).
+Notation "A ==' B" := (egalitate A B) (at level 53, left associativity).
+Notation "A !=' B" := (egalitate A B) (at level 53, left associativity).
+
 
 
 
