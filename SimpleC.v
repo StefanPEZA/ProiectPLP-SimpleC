062d7e94b7ace8e62f8adbbc8e94a954f0d97065
Stefan Pezamosca <stefan2014pe@gmail.com>
2020-12-14 08:17:10 +0200

    Am restructurat expresiile  + modificat cateva notatii/denumiri

--- SimpleC.v
+++ SimpleC.v
@@ -27,38 +27,57 @@ Coercion intVal : Z >-> newInt.
 Coercion natVal : nat >-> newNat.
 Coercion boolVal : bool >-> newBool.
 
-Inductive Exp:=
-| n_const : newNat -> Exp (*constanta de tip nat*)
-| i_const : newInt -> Exp (*constanta de tip int*)
-| b_const : newBool -> Exp (*constanta de tip bool*)
-| avar : string -> Exp (*variabila de tip nat/int/bool*)
-| adunare : Exp -> Exp -> Exp
-| scadere : Exp -> Exp -> Exp
-| inmultire : Exp -> Exp -> Exp
-| impartire : Exp -> Exp -> Exp
-| modulo : Exp -> Exp -> Exp
-| putere : Exp -> Exp -> Exp
-| negatie : Exp -> Exp
-| si_logic : Exp -> Exp -> Exp
-| sau_logic : Exp -> Exp -> Exp
-| mai_mic : Exp -> Exp -> Exp
-| mai_micEq : Exp -> Exp -> Exp
-| mai_mare : Exp-> Exp -> Exp
-| mai_mareEq : Exp -> Exp -> Exp
-| egalitate : Exp -> Exp -> Exp
-| inegalitate : Exp -> Exp -> Exp
-| sau_exclusiv : Exp -> Exp -> Exp
-| si_exclusiv: Exp -> Exp -> Exp
 (*operatii pe string-uri*)
-| s_const : newString -> Exp (*constanta de tip string*)
-| s_concat : newString -> newString -> Exp (*concateneaza doua string-uri*)
-| s_toNat : string -> Exp (*transforma un string in numar natural*)
-| s_toInt : string -> Exp. (*transforma un string in numar intreg*)
+Inductive SExp :=
+| s_const : newString -> SExp (*constanta de tip string*)
+| svar : string -> SExp (*variabila de tip string*)
+| s_concat : newString -> newString -> SExp (*concateneaza doua string-uri*)
+| toString : string -> SExp. (*transorma orice variabila intr-un string*)
 
-Coercion n_const : newNat >-> Exp.
-Coercion i_const : newInt >-> Exp.
-Coercion b_const : newBool >-> Exp.
-Coercion avar : string >-> Exp.
+Inductive AExp:=
+| i_const : newInt -> AExp (*constanta de tip int*)
+| n_const : newNat -> AExp (*constanta de tip nat*)
+| avar : string -> AExp (*variabila de tip nat/int*)
+| adunare : AExp -> AExp -> AExp
+| scadere : AExp -> AExp -> AExp
+| inmultire : AExp -> AExp -> AExp
+| impartire : AExp -> AExp -> AExp
+| modulo : AExp -> AExp -> AExp
+| putere : AExp -> AExp -> AExp
+| toNat : string -> AExp (*transforma orice intr-un numar natural*)
+| toInt : string -> AExp (*transforma orice intr-un intreg*)
+| strLen : SExp -> AExp.
+
+Inductive BExp :=
+| b_const : newBool -> BExp (*constanta de tip bool*)
+| bvar : string -> BExp (*variabila de tipul bool*)
+| negatie : BExp -> BExp
+| si_logic : BExp -> BExp -> BExp
+| sau_logic : BExp -> BExp -> BExp
+| mai_mic : AExp -> AExp -> BExp
+| mai_micEq : AExp -> AExp -> BExp
+| mai_mare : AExp-> AExp -> BExp
+| mai_mareEq : AExp -> AExp -> BExp
+| egalitate : AExp -> AExp -> BExp
+| inegalitate : AExp -> AExp -> BExp
+| sau_exclusiv : BExp -> BExp -> BExp
+| si_exclusiv: BExp -> BExp -> BExp
+| toBool : string -> BExp. (*transforma un numar in boolean*)
+
+Coercion i_const : newInt >-> AExp.
+Coercion n_const : newNat >-> AExp.
+Coercion b_const : newBool >-> BExp.
+Coercion s_const : newString >-> SExp.
+Coercion avar : string >-> AExp.
+Coercion bvar : string >-> BExp.
+Coercion svar : string >-> SExp.
+
+Notation "'Concat(' S1 , S2 )" := (concat S1 S2) (at level 50, left associativity).
+Notation "'ToNat(' S )" := (toNat S) (at level 0).
+Notation "'ToInt(' S )" := (toInt S) (at level 0).
+Notation "'ToBool(' S )" := (toBool S) (at level 0).
+Notation "'ToString(' S )" := (toString S) (at level 0).
+Notation "'StrLen(' S )" := (strLen S) (at level 0).
 
 (*Notatii expresii algebrice*)
 Notation "A +' B" := (adunare A B)(at level 50, left associativity).
@@ -81,9 +100,45 @@ Notation "A >' B" := (mai_mic A B) (at level 52, left associativity).
 Notation "A ==' B" := (egalitate A B) (at level 53, left associativity).
 Notation "A !=' B" := (egalitate A B) (at level 53, left associativity).
 
-Coercion s_const : newString >-> Exp.
-Notation "'concat(' S1 , S2 ')'" := (s_concat S1 S2) (at level 50, left associativity).
-Notation "'strToNat(' S ')'" := (s_toNat S) (at level 0).
+Inductive Stmt := 
+(*in interiorul unei functii se pot declara 
+variabile doar cu o valoare introdusa manual*)
+| decl_nat : string -> AExp -> Stmt (*declarare nat cu o valoare specificata*)
+| decl_int : string -> AExp -> Stmt (*declarare int cu o valoare specificata*)
+| decl_bool : string -> BExp -> Stmt (*declarare bool cu o valoare specificata*)
+| decl_string : string -> BExp -> Stmt (*declarare string cu o valoare specificata*)
+| secventa : Stmt -> Stmt -> Stmt (*secventa de Stmt*)
+| skip : Stmt (*bloc gol e instructiuni*)
+| break : Stmt (*iesi din loop*)
+| continue : Stmt (*treci direct la urmatoarea iteratie*)
+| asig_nr : string -> AExp -> Stmt (*asigneaza o valoare unei variabile de tip numar*)
+| asig_bool : string -> BExp -> Stmt (*asigneaza o valoare unei variabile de tip bool*)
+| asig_str : string -> SExp -> Stmt (*asigneaza o valoare unei variabile de tip string*)
+| citeste : string -> Stmt (*citeste inputul si il pune intr-o variabila*)
+| scrie : newString -> Stmt (*scrie un string*)
+| ifthen : BExp -> Stmt -> Stmt (*if cu o singura ramura*)
+| ifthenelse : BExp -> Stmt -> Stmt -> Stmt (*if cu doua ramuri*)
+| whileloop : BExp -> Stmt -> Stmt (*instructiunea repetitiva while*)
+| dowhile : Stmt -> BExp -> Stmt (*instructiunea repetitiva do-while*)
+| forloop : Stmt -> BExp -> Stmt -> Stmt -> Stmt (*instructiunea repetitiva for*)
+| switch : AExp -> list Cases -> Stmt (*swtch() cu unul sau mai multe cazuri*)
+| apelfunc : string -> list string -> Stmt (*apelul unei functii*)
+(*tip nou pentru case-urile din switch*)
+with Cases :=
+ caseDefault : Stmt -> Cases (*cazul implicit al switch-ului*)
+| caseOther : AExp -> Stmt -> Cases. (*alte cazuri*)
+
+Inductive newType : Type :=
+| err_undeclared : newType (*eroare variabile nedeclarata*)
+| err_assignment : newType (*eroare asignare variabila nedeclarata*)
+| default : newType (*variabila contine valoarea default*)
+| natType : newNat -> newType (*variabila este de tip nat*)
+| intType : newInt -> newType (*variabila este de tip int*)
+| boolType : newBool -> newType (*variabila este de tip bool*)
+| stringType : newString -> newType (*variabila este de tip string*)
+| code : Stmt -> newType. (*codul unei funcii*)
+
+Coercion code : Stmt >-> newType.
 
 (*inafara functiilor se pot declara variabile
  doar cu valoarea default*)
@@ -94,43 +149,7 @@ Inductive Lang :=
 | decl_bool0 : string -> Lang (*declarare bool cu valoare default*)
 | decl_string0 : string -> Lang(*declarare string cu valoare default*)
 | functiaMain : Stmt -> Lang  (*functia principala main*)
-| functie : string -> list string -> Stmt -> Lang (*declarare de functii*)
-(*in interiorul unei functii se pot declara 
-variabile doar cu o valoare introdusa manual*)
-with Stmt := 
- decl_nat : string -> Exp -> Stmt (*declarare nat cu o valoare specificata*)
-| decl_int : string -> Exp -> Stmt (*declarare int cu o valoare specificata*)
-| decl_bool : string -> Exp -> Stmt (*declarare bool cu o valoare specificata*)
-| decl_string : string -> Exp -> Stmt (*declarare string cu o valoare specificata*)
-| secventa : Stmt -> Stmt -> Stmt (*secventa de Stmt*)
-| skip : Stmt (*bloc gol e instructiuni*)
-| break : Stmt (*iesi din loop*)
-| continue : Stmt (*treci direct la urmatoarea iteratie*)
-| asignare : string -> Exp -> Stmt (*asigneaza o valoare unei variabile*)
-| citeste : string -> Stmt (*citeste inputul si il pune intr-o variabila*)
-| scrie : newString -> Stmt (*scrie un string*)
-| ifthen : Exp -> Stmt -> Stmt (*if cu o singura ramura*)
-| ifthenelse : Exp -> Stmt -> Stmt -> Stmt (*if cu doua ramuri*)
-| whileloop : Exp -> Stmt -> Stmt (*instructiunea repetitiva while*)
-| dowhile : Stmt -> Exp -> Stmt (*instructiunea repetitiva do-while*)
-| forloop : Stmt -> Exp -> Stmt -> Stmt -> Stmt (*instructiunea repetitiva for*)
-| switch : Exp -> list Cases -> Stmt (*swtch() cu unul sau mai multe cazuri*)
-| apelfunc : string -> list string -> Stmt (*apelul unei functii*)
-(*tip nou pentru case-urile din switch*)
-with Cases :=
- caseDefault : Stmt -> Cases (*cazul implicit al switch-ului*)
-| caseOther : Exp -> Stmt -> Cases. (*alte cazuri*)
-
-Print list.
-
-Inductive newType : Type :=
-| err_undeclared : newType (*eroare variabile nedeclarata*)
-| err_assignment : newType (*eroare asignare variabila nedeclarata*)
-| default : newType (*variabila contine valoarea default*)
-| natType : newNat -> newType (*variabila este de tip nat*)
-| intType : newInt -> newType (*variabila este de tip int*)
-| boolType : newBool -> newType (*variabila este de tip bool*)
-| stringType : newString -> newType. (*variabila este de tip string*)
+| functie : string -> list string -> Stmt -> Lang. (*declarare de functii*)
 
 Notation "'uint0'' V" := (decl_nat0 V) (at level 90, right associativity).
 Notation "'uint'' V <-- E" := (decl_nat V E) (at level 90, right associativity).
@@ -144,7 +163,9 @@ Notation "'bool'' V <-- E" := (decl_bool V E) (at level 90, right associativity)
 Notation "'string0'' V" := (decl_string0 V) (at level 90, right associativity).
 Notation "'string'' V <-- E" := (decl_string V E) (at level 90, right associativity).
 
-Notation "V ::= E" := (asignare V E) (at level 90, right associativity).
+Notation "V :N= E" := (asig_nr V E) (at level 90, right associativity).
+Notation "V :B= E" := (asig_bool V E) (at level 90, right associativity).
+Notation "V :S= E" := (asig_str V E) (at level 90, right associativity).
 
 Notation "S1 ;; S2" := (secventa S1 S2) (at level 93, right associativity).
 Notation "S1 ;' S2" := (secvLang S1 S2) (at level 93, right associativity).
@@ -152,30 +173,32 @@ Notation "S1 ;' S2" := (secvLang S1 S2) (at level 93, right associativity).
 Notation "'if'' ( B ) 'then'' { S } 'end''" := (ifthen B S) (at level 97).
 Notation "'if'' ( B ) 'then'' { S1 } 'else'' '{' S2 '}' 'end''" := (ifthenelse B S1 S2) (at level 97).
 
-Notation "'while'' ( B ) 'do'' { S } 'end''" := (whileloop B S) (at level 97).
-Notation "'do'' { S } 'while'' ( B ) 'end''" := (dowhile S B) (at level 97).
-Notation "'for'' ( I # B # A ) 'do'' { S } 'end''" := (forloop I B A S) (at level 97).
+Notation "'while'' ( B ) 'do'' { S }" := (whileloop B S) (at level 97).
+Notation "'do'' { S } 'while'' ( B )" := (dowhile S B) (at level 97).
+Notation "'for'' ( I ; B ; A ) 'do'' { S }" := (forloop I B A S) (at level 97).
 
 Notation "'default' : { S };" := (caseDefault S) (at level 96).
 Notation "'case' ( E ):{ S };" := (caseOther E S) (at level 96).
-Notation "'switch'' ( E ){ C1 } 'end''" := (switch E (cons C1 nil)) (at level 97).
-Notation "'switch'' ( E ){ C1 C2 .. Cn  } 'end''" := (switch E (cons C1 (cons C2 .. (cons Cn nil) ..) ) ) (at level 97).
+Notation "'switch'' ( E ){ C1 .. Cn  } 'end''" := (switch E (cons C1 .. (cons Cn nil) .. )) (at level 97).
 
-Notation "'func' 'main' (()){} 'end''" := (functiaMain skip)(at level 95).
-Notation "'func' 'main' (()){ S } 'end''" := (functiaMain S)(at level 95). 
+Notation "'void' 'main' () { }" := (functiaMain skip)(at level 95).
+Notation "'void' 'main' () { S }" := (functiaMain S)(at level 95). 
 
-Notation "'func' N (()){} 'end''" := (functie N nil skip)(at level 95).
-Notation "'func' N (()){ S } 'end''" := (functie N nil S)(at level 95).
-Notation "'func' N (( A )){} 'end''" := (functie N (cons A nil) skip)(at level 95).
-Notation "'func' N (( A1 , A2 , .. , An )){} 'end''" := (functie N (cons A1 (cons A2 .. (cons An nil) .. ) ) skip)(at level 95).
-Notation "'func' N (( A1 , A2 , .. , An )){ S } 'end''" := (functie N (cons A1 (cons A2 .. (cons An nil) .. ) ) S)(at level 95).
+Notation "'void' N () { }" := (functie N nil skip)(at level 95).
+Notation "'void' N () { S }" := (functie N nil S)(at level 95).
+Notation "'void' N (( A1 , .. , An )) { }" := (functie N (cons A1 .. (cons An nil) .. ) skip)(at level 95).
+Notation "'void' N (( A1 , .. , An )) { S }" := (functie N (cons A1 .. (cons An nil) .. ) S)(at level 95).
 
-Notation "'call' N (())" := (apelfunc N nil) (at level 89).
-Notation "'call' N (( A ))" := (apelfunc N (cons A nil)) (at level 89).
-Notation "'call' N (( A1 , A2 , .. , An ))" := (apelfunc N (cons A1 (cons A2 .. (cons An nil) .. ) ) ) (at level 89).
+Notation "'call' N (( A1 , .. , An ))" := (apelfunc N (cons A1 .. (cons An nil) .. ) ) (at level 89).
+Notation "'call' N (( ))" := (apelfunc N nil) (at level 89).
 
 Notation "'write(' S )" := (scrie S) (at level 92).
 Notation "'read(' V )" := (citeste V) (at level 92).
+
+Check void main(){
+         uint' "a" <-- 0 ;;
+         bool' "x" <-- ToBool("a")
+      }.
 
 
 
